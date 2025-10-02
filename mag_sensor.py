961bd959a06f2fd6499c9964a170f08e66100ea2
mmjextreme <mmanjarres@extreme.com.co>
2025-10-01 21:00:05 -0500

    Corrección de la unidad de medida del caudal

--- mag_sensor.py
+++ mag_sensor.py
@@ -129,7 +129,7 @@ def main():
             logger.info("=" * 60)
             logger.info(f"DATOS DEL SENSOR {Config.SENSOR_VERSION.value}")
             logger.info("=" * 60)
-            logger.info(f"💧 CAUDAL:\t{caudal_value:>10.3f} L/min")
+            logger.info(f"💧 CAUDAL:\t{caudal_value:>10.3f} L/s")
             logger.info(f"📏 VOLUMEN:\t{volumen_value:>10.2f} M3")
             logger.info(f"🕐 TIMESTAMP:\t{time.strftime('%Y-%m-%d %H:%M:%S')}")
             logger.info("=" * 60)
